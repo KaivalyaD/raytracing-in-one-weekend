4742d803a1ae334f46b3fb4bf5962714d16fd423
KaivalyaD <kaivalya.v.deshpande@gmail.com>
2023-06-28 16:14:49 +0530

    added defocus blur (dof) to camera

--- include/camera.h
+++ include/camera.h
@@ -9,9 +9,15 @@ private:
     point3 lower_left;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u, v, w;
+    float lens_radius;
 
 public:
-    camera(float viewport_width, float viewport_height, float focal_length) {
+    camera(float viewport_width, float viewport_height, float focal_length)
+    : u(vec3(0.0f, 0.0f, 0.0f)),
+      v(vec3(0.0f, 0.0f, 0.0f)),
+      w(vec3(0.0f, 0.0f, 0.0f)),
+      lens_radius(0.0f) {
         origin = point3(0.0f, 0.0f, 0.0f);
         horizontal = vec3(viewport_width, 0.0f, 0.0f);
         vertical = vec3(0.0f, viewport_height, 0.0f);
@@ -19,7 +25,11 @@ public:
     }
 
     // adjustable vertical field-of-view
-    camera(float vfov, float aspect_ratio) {
+    camera(float vfov, float aspect_ratio)
+    : u(vec3(0.0f, 0.0f, 0.0f)),
+      v(vec3(0.0f, 0.0f, 0.0f)),
+      w(vec3(0.0f, 0.0f, 0.0f)),
+      lens_radius(0.0f) {
         auto theta = degrees_to_radians(vfov);
         auto h = tanf(theta / 2.0f);
         auto viewport_height = 2.0f * h;
@@ -33,15 +43,16 @@ public:
         lower_left = origin - (horizontal / 2.0f) - (vertical / 2.0f) - vec3(0.0f, 0.0f, focal_length);
     }
 
-    camera(point3 position, point3 center, vec3 up, float vfov, float aspect_ratio) {
+    camera(point3 position, point3 center, vec3 up, float vfov, float aspect_ratio)
+    : lens_radius(0.0f) {
         auto theta = degrees_to_radians(vfov);
         auto h = tanf(theta / 2.0f);
         auto viewport_height = 2.0f * h;
         auto viewport_width = aspect_ratio * viewport_height;
 
-        auto w = unit_vector(position - center);
-        auto u = unit_vector(cross(up, w));
-        auto v = cross(u, w);
+        w = unit_vector(position - center);
+        u = unit_vector(cross(up, w));
+        v = cross(u, w);
 
         origin = position;
         horizontal = viewport_width * u;
@@ -49,8 +60,38 @@ public:
         lower_left = origin - horizontal / 2.0f - vertical / 2.0f - w;
     }
 
+    camera(point3 position, point3 center, vec3 up, float vfov, float aspect_ratio, float aperture, float focus_distance) {
+        auto theta = degrees_to_radians(vfov);
+        auto h = tanf(theta / 2.0f);
+        auto viewport_height = 2.0f * h;
+        auto viewport_width = aspect_ratio * viewport_height;
+
+        w = unit_vector(position - center);
+        u = unit_vector(cross(up, w));
+        v = cross(u, w);
+
+        origin = position;
+        horizontal = focus_distance * viewport_width * u;
+        vertical = focus_distance * viewport_height * v;
+        lower_left = origin - horizontal / 2.0f - vertical / 2.0f - focus_distance * w;
+    
+        lens_radius = aperture / 2.0f;
+    }
+
     ray get_ray(float s, float t) const {
-        return ray(origin, lower_left + s * horizontal + t * vertical - origin);
+        auto random_in_unit_disc = [] () {
+            while(true) {
+                auto p = vec3(random_float(-1.0f, 1.0f), random_float(-1.0f, 1.0f), 0.0f);
+                if(p.length_squared() >= 1.0f) continue;
+                return p;
+            }
+        };
+        vec3 offset = vec3(0.0f, 0.0f, 0.0f);
+        if(lens_radius > 0.0f) {
+            vec3 rd = lens_radius * random_in_unit_disc();
+            offset = u * rd.x() + v * rd.y();
+        }
+        return ray(origin + offset, lower_left + s * horizontal + t * vertical - origin - offset);
     }
 };
 
