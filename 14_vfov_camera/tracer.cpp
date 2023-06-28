9a38fdd64a4a47bbbb5faeaa94bc063234ceaa83
KaivalyaD <kaivalya.v.deshpande@gmail.com>
2023-06-28 14:50:55 +0530

    added vfov support for camera

---
+++ 14_vfov_camera/tracer.cpp
@@ -0,0 +1,70 @@
+#include"../include/rtweekend.h"
+
+#include"../include/vec3_color_ext.h"
+#include"../include/hittable_list.h"
+#include"../include/sphere.h"
+#include"../include/camera.h"
+#include"../include/material.h"
+
+#include<iostream>
+
+color3 ray_color(const ray& r, const hittable_list& world, int depth) {
+    hit_record rec;
+
+    // if ray bounce limit exceeded, return
+    if(depth <= 0)
+        return color3(0.0f, 0.0f, 0.0f);
+
+    if(world.hit(r, 0.001f, infinity, rec)) {
+        ray scattered;
+        color3 attenuation;
+        if(rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * ray_color(scattered, world, depth - 1);
+        }
+        return color3(0.0f, 0.0f, 0.0f);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5f * unit_direction.y() + 0.5f;
+    return (1.0f - t) * color3(1.0f, 1.0f, 1.0f) + t * color3(0.5f, 0.7f, 1.0f);
+}
+
+int main(void) {
+    using namespace std;
+    
+    // image
+    const auto aspect_ratio = 16.0f / 9.0f;
+    const int image_width = 1024;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
+
+    // world
+    auto R = cosf(pi / 4.0f);
+    hittable_list world;
+
+    auto material_blue = make_shared<lambertian>(color3(0.0f, 0.0f, 1.0f));
+    auto material_red = make_shared<lambertian>(color3(1.0f, 0.0f, 0.0f));
+
+    world.add(make_shared<sphere>(point3(-R, 0.0f, -1.0f), R, material_blue));
+    world.add(make_shared<sphere>(point3( R, 0.0f, -1.0f), R, material_red));
+
+    // camera
+    camera cam = camera(132.5f, aspect_ratio);
+
+    // render
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for(int j = image_height-1; j >= 0; --j) {
+        clog << "\rScanlines remaining: " << j << ' ' << flush;
+        for(int i = 0; i < image_width; ++i) {
+            color3 pixel_color(0.0f, 0.0f, 0.0f);
+            for(int s = 0; s < samples_per_pixel; ++s) {
+                auto u = float(i + random_float()) / (image_width - 1);
+                auto v = float(j + random_float()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            write_color(cout, pixel_color, samples_per_pixel);
+        }
+    }
+    clog << "\nDone.\n";
+}
--- include/camera.h
+++ include/camera.h
@@ -18,6 +18,21 @@ public:
         lower_left = origin - (horizontal / 2.0f) - (vertical / 2.0f) - vec3(0.0f, 0.0f, focal_length);
     }
 
+    // adjustable vertical field-of-view
+    camera(float vfov, float aspect_ratio) {
+        auto theta = degrees_to_radians(vfov);
+        auto h = tanf(theta / 2.0f);
+        auto viewport_height = 2.0f * h;
+        auto viewport_width = aspect_ratio * viewport_height;
+
+        auto focal_length = 1.0f;
+
+        origin = point3(0.0f, 0.0f, 0.0f);
+        horizontal = vec3(viewport_width, 0.0f, 0.0f);
+        vertical = vec3(0.0f, viewport_height, 0.0f);
+        lower_left = origin - (horizontal / 2.0f) - (vertical / 2.0f) - vec3(0.0f, 0.0f, focal_length);
+    }
+
     ray get_ray(float u, float v) const {
         return ray(origin, lower_left + u * horizontal + v * vertical - origin);
     }
